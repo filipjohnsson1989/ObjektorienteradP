a379be120415f90ed6abec0808cc2b77392c750b
Filip Johnsson <Filip.johnsson1989@gmail.com>
2021-12-10 21:35:29 +0100

    Custom exception handler for validation. No using.

---
+++ ObjektorienteradP.Common/ValidationException.cs
@@ -0,0 +1,10 @@
+﻿namespace ObjektorienteradP.Common;
+
+[Serializable()]
+public class ValidationException : System.Exception
+{
+    public ValidationException() : base() { }
+    public ValidationException(string message) : base(message) { }
+    public ValidationException(string message, Exception inner) : base(message, inner) { }
+    public ValidationException(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+}
