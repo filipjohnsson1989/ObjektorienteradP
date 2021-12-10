e23488cbb9454854213e5f88f94ae1a467f21174
Filip Johnsson <Filip.johnsson1989@gmail.com>
2021-12-10 21:33:22 +0100

    encapsulation and validation through properties setters

--- ObjektorienteradP.DL/Person.cs
+++ ObjektorienteradP.DL/Person.cs
@@ -1,17 +1,73 @@
-﻿namespace ObjektorienteradP.DL;
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ObjektorienteradP.DL;
 
 public class Person
 {
     private uint age;
+    public uint Age
+    {
+        get => this.age;
+        set
+        {
+            if (value <= 0)
+                throw new ArgumentException("Ålder kan bara tilldelas ett värde större än 0", "Ålder");
+
+            this.age = value;
+        }
+    }
+
     private string? fName;
+    public string? FName
+    {
+        get => this.fName;
+        set
+        {
+            if (value is null || value.Length < 2 || value.Length > 10)
+                throw new ArgumentException("Förnamn kan bara vara mellan 2 och 10 tecken");
+
+            this.fName = value;
+        }
+    }
+
     private string? lName;
-    private uint height;
-    private uint weight;
+    public string? LName
+    {
+        get => this.lName;
+        set
+        {
+            if (value is null || value.Length < 3 || value.Length > 15)
+                throw new ArgumentException("Efternamn kan bara vara mellan 3 och 15 tecken");
 
-    public int Age { get; set; }
-    public string? FName { get; set; }
-    public string? LName { get; set; }
-    public uint Height { get; set; }
-    public uint Weight { get; set; }
+            this.lName = value;
+        }
+    }
 
+    public string Name { get => $"{FName} {lName}"; }
+
+    private int height;
+    public int Height
+    {
+        get => height;
+        set => height = value;
+    }
+
+    private int weight;
+    public int Weight
+    {
+        get => weight;
+        set => weight = value;
+    }
+
+    public Person(uint age, string fName, string lName)
+    {
+        Age = age;
+        FName = fName;
+        LName = lName;
+    }
+
+    public override string ToString()
+    {
+        return $"{this.Name} är {Age} år gammal, {height}cm lång och {weight} kilo";
+    }
 }
--- ObjektorienteradP.UI.Console/Program.cs
+++ ObjektorienteradP.UI.Console/Program.cs
@@ -3,14 +3,33 @@ using ObjektorienteradP.DL;
 
 Console.WriteLine("Objektorienterad programmering!");
 
-#region Inkapsling1
+#region Inkapsling 1.1
 // Kommer du direktåt variablerna?
-// Ja, det gör jag.
-Person person = new Person();
-person.Age = 10;
-Console.WriteLine(person.Age); // Output: 10
+Person person = new(age: 32, fName: "Filip", lName: "Johnsson");
 
+// Man kammer inte åt de variabelen som privata fält.
+//person.age = 2; // Compiler Error: 'Person.age' is inaccessible due to its protection level
 
+// Man kammer bara direkt åt publika properties.
+person.Height = 172;
+Console.WriteLine(person.Height); // Output: 172
+
+try
+{
+    //person.Age = 0;
+    //person.FName = string.Empty;
+    person.LName = "";
+
+}
+catch (ArgumentException ex) when (ex.ParamName == "Ålder")
+{
+    Console.WriteLine(ex.Message);
+    Console.WriteLine(person.ToString());
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
 
 #endregion Inkapsling1
 
