6d073253421a5d2a9258f88b36dd86471019394b
Filip Johnsson <Filip.johnsson1989@gmail.com>
2021-12-10 17:49:12 +0100

    Add DL

---
+++ ObjektorienteradP.DL/Person.cs
@@ -0,0 +1,17 @@
+﻿namespace ObjektorienteradP.DL;
+
+internal class Person
+{
+    private uint age;
+    private string? fName;
+    private string? lName;
+    private uint height;
+    private uint weight;
+
+    public int Age { get; set; }
+    public string? FName { get; set; }
+    public string? LName { get; set; }
+    public uint Height { get; set; }
+    public uint Weight { get; set; }
+
+}
