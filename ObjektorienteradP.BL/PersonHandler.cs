018422f3ce26ea3c867859a53115832cfd83a638
Filip Johnsson <Filip.johnsson1989@gmail.com>
2021-12-11 22:39:37 +0100

    A feature to edit several properties of person through one method in person handler

--- ObjektorienteradP.BL/PersonHandler.cs
+++ ObjektorienteradP.BL/PersonHandler.cs
@@ -6,12 +6,12 @@ public class PersonHandler
 {
     #region Inkapsling2
     public void SetAge(Person person, uint age) => person.Age = age;
-    #endregion Inkapsling2
+    #endregion 
 
     #region Inkapsling3
     public Person CreatePerson(uint age, string fName, string lName, double height, double weight) =>
         new Person(age: age, fName: fName, lName: lName, height: height, weight: weight);
-    #endregion Inkapsling3
+    #endregion 
 
     #region Inkapsling4
     public void SetFName(Person person, string fName) => person.FName = fName;
@@ -21,5 +21,23 @@ public class PersonHandler
     public void SetHeight(Person person, double height) => person.Height = height;
 
     public void SetWeight(Person person, double weight) => person.Weight = weight;
-    #endregion Inkapsling4
+
+    /// <summary>
+    /// It edits info of desire person.
+    /// </summary>
+    /// <param name="person">Object of the desire person</param>
+    /// <param name="age">Set desire age otherwise leave it null to have current data.</param>
+    /// <param name="fName">Set desire first name otherwise leave it null to have current data.</param>
+    /// <param name="lName">Set desire last name otherwise leave it null to have current data.</param>
+    /// <param name="height">Set desire height otherwise leave it null to have current data.</param>
+    /// <param name="weight">Set desire weight otherwise leave it null to have current data.</param>
+    public void SetPerson(Person person, uint? age, string? fName, string? lName, double? height, double? weight)
+    {
+        if(age.HasValue) SetAge(person, age.Value);
+        if(fName is not null) SetFName(person, fName);
+        if(lName is not null) SetLName(person, lName);
+        if(height.HasValue) SetHeight(person, height.Value);
+        if(weight.HasValue) SetWeight(person, weight.Value);
+    }
+    #endregion 
 }
--- ObjektorienteradP.UI.Console/Program.cs
+++ ObjektorienteradP.UI.Console/Program.cs
@@ -1,60 +1,68 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ObjektorienteradP.BL;
-using ObjektorienteradP.DL;
+//using ObjektorienteradP.DL;
 
 
 Console.WriteLine("Objektorienterad programmering!");
 
 #region Inkapsling1
 // Kommer du direktåt variablerna?
-Person filip = new() { Age = 32, FName = "Filip", LName = "Johnsson" };
+//Person filip = new() { Age = 32, FName = "Filip", LName = "Johnsson" };
 
 // Man kammer inte åt de variabelen som privata fält.
 //person.age = 2; // Compiler Error: 'Person.age' is inaccessible due to its protection level
 
 // Man kammer bara direkt åt publika properties.
-filip.Height = 172.5;
-Console.WriteLine(filip.Height); // Output: 172.5
+//filip.Height = 172.5;
+//Console.WriteLine(filip.Height); // Output: 172.5
+
+//try
+//{
+//    //filip.Age = 0;
+//    //person.FName = string.Empty;
+//    //person.LName = "";
+//}
+//catch (ArgumentException ex) when (ex.ParamName == "Ålder")
+//{
+//    Console.WriteLine(ex.Message);
+//    Console.WriteLine(filip.ToString());
+//}
+//catch (Exception ex)
+//{
+//    Console.WriteLine(ex.Message);
+//}
+#endregion 
+
 
 try
 {
-    person.Age = 0;
-    //person.FName = string.Empty;
-    //person.LName = "";
-}
-catch (ArgumentException ex) when (ex.ParamName == "Ålder")
-{
-    Console.WriteLine(ex.Message);
-    Console.WriteLine(filip.ToString());
+    #region Inkapsling2
+    // abstraherat med två lager
+    PersonHandler personHandler = new();
+    //personHandler.SetAge(filip, 32);
+    //Console.WriteLine(filip.ToString());
+    #endregion 
+
+    #region Inkapsling3
+    var filippa = personHandler.CreatePerson(age: 35, fName: "filippa", lName: "johnsson", height: 164.5, weight: 64.5);
+    Console.WriteLine(filippa.ToString());
+    #endregion 
+
+    #region Inkapsling5
+    personHandler.SetAge(filippa, 34);
+    personHandler.SetFName(filippa, "Filippa");
+    personHandler.SetLName(filippa, "Johnsson");
+    personHandler.SetHeight(filippa, 164);
+    personHandler.SetWeight(filippa, 64);
+    Console.WriteLine(filippa.ToString());
+
+    personHandler.SetPerson(filippa, age: 33, fName: "Ms Filippa", lName: null, height: 165, weight: null);
+    Console.WriteLine(filippa.ToString());
+    #endregion 
+
+    Console.ReadLine();
 }
 catch (Exception ex)
 {
     Console.WriteLine(ex.Message);
 }
-#endregion Inkapsling1
-
-
-#region Inkapsling2
-// abstraherat med två lager
-PersonHandler personHandler = new();
-personHandler.SetAge(filip, 32);
-Console.WriteLine(filip.ToString());
-#endregion Inkapsling2
-
-
-#region Inkapsling3
-var filippa = personHandler.CreatePerson(age : 35, fName : "filippa", lName : "johnsson", height: 164.5, weight: 64.5);
-Console.WriteLine(filippa.ToString());
-#endregion Inkapsling3
-
-#region Inkapsling5
-personHandler.SetAge(filippa, 34);
-personHandler.SetFName(filippa, "Filippa");
-personHandler.SetLName(filippa, "Johnsson");
-personHandler.SetHeight(filippa, 164);
-personHandler.SetWeight(filippa, 64);
-Console.WriteLine(filippa.ToString());
-#endregion Inkapsling5
-
-
-Console.ReadLine();
